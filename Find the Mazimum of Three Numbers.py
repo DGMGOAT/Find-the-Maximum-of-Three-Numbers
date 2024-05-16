32e984015eac49dcec42ff08252fe3ecac918bef
DGMGOAT <mehtadhyang@gmail.com>
2024-05-15 17:31:52 -0700

    Add files via upload

---
+++ Find the Mazimum of Three Numbers.py
@@ -0,0 +1,10 @@
+a=int(input("Type the first number you could think of."))
+b=int(input("Now type the second number."))
+c=int(input("Finally, type the third number."))
+if (a>b) and (a>c):
+    print("This is the maximum number:",d)
+elif (b>c) and (b>a):
+    print("This is your maximum number:",b )
+else:
+    print("This is your maximum number:",a)
+
